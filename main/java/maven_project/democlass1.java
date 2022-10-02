b4100247d39bcebb71ca49969408880ef70cb8ce
sumithra0900 <sumithra0907ar@gmail.com>
2022-10-02 16:54:33 +0100

    new

---
+++ main/java/maven_project/democlass1.java
@@ -0,0 +1,7 @@
+package maven_project;
+
+public class democlass1 {
+	public String Samplemethod() {
+		return "welcome doker";
+	}
+}
---
+++ test/java/maven_project/demotest1.java
@@ -0,0 +1,14 @@
+package maven_project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class demotest1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
